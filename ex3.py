186de8e2ec37475b7a2df2e974bd35480cf4effc
Lauren <lauren.timzen@gmail.com>
2019-08-31 04:55:52 -0700

    explore PEMDAS

---
+++ ex3.py
@@ -0,0 +1,32 @@
+# prints text
+print "I will now count my chickens:"
+
+#prints text, then prints the math using PEMDAS
+print "Hens", float (25 + 30 / 6)
+# prints text, then prints the math using PEMDAS
+print "Roosters", float (100 - 25 * 3 % 4)
+
+#Prints text
+print "Now I will count the eggs:"
+
+# prints/calculates the math using PEMDAS
+print float (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# prints the question
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# does the math and determines if true or false
+print 3 + 2 < 5 - 7
+
+# prints text and the math
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# determines if the math is true or false
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
