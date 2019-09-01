a2a97446b56a9594c71e2b93bd1b6266312fdc81
Lauren <lauren.timzen@gmail.com>
2019-08-31 17:38:51 -0700

    pushing folders practice

---
+++ ex46/skeleton/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = [
+    'description': 'My Project',
+    'author': 'Lauren Timzen',
+    'url': 'https://github.com/laurentimzen/python_learning',
+    'author_email': 'lauren.timzen@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+]
+
+setup(**config)
